4eaa9855de9678b87be0a208db5a01c177b7e15a
Ignacio Zanotto <ignaciozanotto@gmail.com>
2022-08-16 17:19:21 -0300

    task managers no frontend

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  delete 'tasks/:id', to: 'tasks#destroy', as: 'destroy'
+
+
 end
